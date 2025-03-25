4412469838dc7225e5a33443532defa5733a3a12
Hoang Tien <143521414+hoangtien1105@users.noreply.github.com>
2025-03-25 14:37:24 +0700

    test ci cd new controller

---
+++ APIProject/Controllers/testController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class testController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAction()
+        {
+            return Ok("dmm swagger");
+        }
+    }
+}
--- APIProject/Program.cs
+++ APIProject/Program.cs
@@ -14,12 +14,13 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline.
 //if (app.Environment.IsDevelopment())
 //{
     app.UseSwagger();
     app.UseSwaggerUI();
 //}
+
 
 app.UseAuthorization();
 
